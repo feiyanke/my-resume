4a64be1c709a1369f2ecec410b67fe28d965996b
feiyanke <feiyanke@gmail.com>
2018-06-08 10:16:05 +0800

    ~~

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-hackmyresume build resume.json TO out/resume.all -t node_modules/jsonresume-theme-caffeine
+hackmyresume build resume.json TO out/resume.all -t node_modules/jsonresume-theme-caffeine -o options.json
